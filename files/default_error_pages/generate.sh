08a3a676461b370b9512b6eb1b5e2061c5fa38c0
TipiiP <theo.palhol@gmail.com>
2021-04-19 21:41:42 +0200

    error page generator

---
+++ files/default_error_pages/generate.sh
@@ -0,0 +1,8 @@
+if [ "$#" -ne 2 ]
+then
+	echo 'Error : Need two args. Usage : ./generate error_code error_message'
+
+else
+	sed "s/{{error_code}}/$1/g; s/{{error_message}}/$2/g" ./template.html > ./custom_$1.html
+	echo "error_$1.html has been generated"
+fi
